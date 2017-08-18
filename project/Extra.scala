34b47e77e0b9b6b2a776c358f36ae154581ee953
Denys Shabalin <denys.shabalin@epfl.ch>
2017-08-18 13:44:44 +0200

    Bump to 0.2.3-SNAPSHOT

--- project/Extra.scala
+++ project/Extra.scala
@@ -10,7 +10,7 @@ object Extra {
 
   val sbtPluginSettings = ScriptedPlugin.scriptedSettings ++ Seq(
       organization := "org.scala-native",
-      version := "0.2.2",
+      version := "0.2.3-SNAPSHOT",
       sbtPlugin := true,
       scriptedLaunchOpts ++= Seq(
         "-Dplugin.version=" + version.value,

3c730c20c44dce65bee06e658e932a9da2b13402
Denys Shabalin <denys.shabalin@epfl.ch>
2018-03-26 14:45:31 +0200

    Fix #83: Move `JSPlatform` back to `scalajscrossproject`

--- sbt-scalajs-crossproject/src/main/scala/scalajscrossproject/JSPlatform.scala
+++ sbt-scalajs-crossproject/src/main/scala/scalajscrossproject/JSPlatform.scala
@@ -1,11 +1,8 @@
-/* It is totally evil to introduce a class in the package of sbt-scalajs, but that is
- * the only way we can be forward source compatible with Scala.js 1.x.
- */
-package org.scalajs.sbtplugin
+package scalajscrossproject
 
 import sbtcrossproject._
-
 import sbt._
+import org.scalajs.sbtplugin._
 
 case object JSPlatform extends Platform {
   def identifier: String                = "js"

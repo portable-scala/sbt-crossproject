8e62b58c2e70e1074e2129beec8c3253f16b9e33
Denys Shabalin <denys.shabalin@epfl.ch>
2018-03-29 15:29:43 +0200

    Drop redundant code

--- sbt-scala-native-crossproject/src/main/scala/scalanativecrossproject/ScalaNativeCrossPlugin.scala
+++ sbt-scala-native-crossproject/src/main/scala/scalanativecrossproject/ScalaNativeCrossPlugin.scala
@@ -1,14 +1,5 @@
 package scalanativecrossproject
 
-import sbt._, Keys._
-import scalanative.sbtplugin._
-import org.portablescala.sbtplatformdeps.PlatformDepsPlugin.autoImport._
-
-import scala.language.implicitConversions
-
-object ScalaNativeCrossPlugin extends AutoPlugin {
-  override def trigger           = allRequirements
-  override def requires: Plugins = ScalaNativePlugin
-
+object ScalaNativeCrossPlugin extends sbt.AutoPlugin {
   object autoImport extends ScalaNativeCross
 }

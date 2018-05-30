f65ad3a1fadf86cb4711405f6a96db6fea384b20
Sébastien Doeraene <sjrdoeraene@gmail.com>
2018-05-30 13:19:53 +0200

    Simplify the public API by removing traits.
    Instead of defining traits whose only purpose was to be mixed in
    the relevant `autoImport` objects, we define their members directly
    inside the `autoImport`s.
    
    This highlighted that the recommended compatibility imports were
    
      import sbtcrossproject.{crossProject, CrossType}
    
    whereas in fact they should always have been
    
      import sbtcrossproject.CrossPlugin.autoImport.{crossProject, CrossType}
    
    instead. This commit also adapts the readme accordingly. Deprecated
    members in the package object maintain source compatibility.

--- sbt-scala-native-crossproject/src/main/scala/scalanativecrossproject/ScalaNativeCrossPlugin.scala
+++ sbt-scala-native-crossproject/src/main/scala/scalanativecrossproject/ScalaNativeCrossPlugin.scala
@@ -1,5 +1,28 @@
 package scalanativecrossproject
 
+import scala.language.implicitConversions
+
+import sbt._
+import sbtcrossproject._
+
 object ScalaNativeCrossPlugin extends sbt.AutoPlugin {
-  object autoImport extends ScalaNativeCross
+  object autoImport {
+
+    val NativePlatform = scalanativecrossproject.NativePlatform
+
+    implicit def NativeCrossProjectBuilderOps(
+        builder: CrossProject.Builder): NativeCrossProjectOps =
+      new NativeCrossProjectOps(builder.crossType(CrossType.Full))
+
+    implicit class NativeCrossProjectOps(project: CrossProject) {
+      def native: Project = project.projects(NativePlatform)
+
+      def nativeSettings(ss: Def.SettingsDefinition*): CrossProject =
+        nativeConfigure(_.settings(ss: _*))
+
+      def nativeConfigure(transformer: Project => Project): CrossProject =
+        project.configurePlatform(NativePlatform)(transformer)
+    }
+
+  }
 }
